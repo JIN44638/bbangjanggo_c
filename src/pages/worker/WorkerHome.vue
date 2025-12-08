9420bc713d17d164ee73a6ac8c811f7b861baf6a
13inuo0 <13inuo0@github.com>
2025-12-08 13:20:55 +0900

    하단 메뉴 간격 수정

--- src/pages/worker/WorkerHome.vue
+++ src/pages/worker/WorkerHome.vue
@@ -112,14 +112,12 @@
     </div>
 
     <!-- 하단 메뉴바 -->
-    <nav
-      class="z-[9999] flex items-center justify-between bg-white w-full h-[60px] py-4 px-4 md:px-[35px] shadow-[0px_-1px_5px_rgba(0,0,0,0.05)]"
-    >
+    <nav class="z-[9999] flex bg-white w-full h-[60px] shadow-[0px_-1px_5px_rgba(0,0,0,0.05)]">
       <router-link
         :to="link.path"
         v-for="link in links"
         :key="link.path"
-        class="flex flex-col items-center justify-center gap-1 transition-colors"
+       class="flex-1 flex flex-col items-center justify-center gap-1 transition-colors text-center"
         @click="activateMenu(link.path)"
       >
         <i :class="[link.icon, 'text-2xl', activeMenu === link.path ? 'text-[#50311D]' : 'text-[#969696]']"></i>
@@ -134,7 +132,7 @@
       <!-- 서브메뉴 버튼 -->
       <button
         @click="toggleSubMenu"
-        class="flex flex-col items-center justify-center gap-1 transition-colors cursor-pointer"
+        class="flex-1 flex flex-col items-center justify-center gap-1 transition-colors cursor-pointer text-center"
         :class="isSubMenuOpen ? 'text-[#50311D]' : 'text-gray-400'"
       >
         <i class="fa-solid fa-bars text-2xl"></i>
